26cdbcfc6e9b4032f2b4875a89debec3ec9c93a9
“rzbar” <202205501711@smail.xtu.edu.cn>
2024-07-19 16:46:50 +0800

    把ui都做成了prefab

--- Assets/Scripts/CampsiteScripts/ShowToolUI.cs
+++ Assets/Scripts/CampsiteScripts/ShowToolUI.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+/// <summary>
+/// 玩家打开工具面板
+/// </summary>
 public class ShowToolUI : PlayerInteraction
 {
     public GameObject UIPannel;
