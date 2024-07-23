bce79726d1d7e67cca2bf3c2376d936963d180f4
YaeSakura <1152438234@qq.com>
2024-07-23 14:14:20 +0800

    编写ProjectBase代码文档，为SceneMgr的脚本添加异步卸载场景功能

--- Assets/Scripts/ProjectBase/Scene/SceneMgr.cs
+++ Assets/Scripts/ProjectBase/Scene/SceneMgr.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using ProjectBase.Event;
 using ProjectBase.Mono;
 using ProjectBase.UI;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
@@ -45,6 +46,29 @@ namespace ProjectBase.Scene
                 yield return res.progress;
             }
             UIManager.Instance.HidePanel("loadingPanel");
+            callback?.Invoke();
+        }
+
+
+        /// <summary>
+        /// 异步卸载场景，bool判断是否卸载当前场景中所有的物体，默认为卸载所有
+        /// </summary>
+        /// <param name="sceneName"></param>
+        /// <param name="callback"></param>
+        /// <param name="isAllObjectUnload"></param>
+        public void UnloadSceneAsync(string sceneName, UnityAction callback, bool isAllObjectUnload = false)
+        {
+            MonoBehaviourController.Instance.StartCoroutine(IEUnloadSceneAsync(sceneName, callback, isAllObjectUnload));
+        }
+        private IEnumerator IEUnloadSceneAsync(string sceneName,UnityAction callback, bool isAllObjectUnload = false)
+        {
+            AsyncOperation asyncOperation = SceneManager.UnloadSceneAsync(sceneName,
+                isAllObjectUnload ? UnloadSceneOptions.UnloadAllEmbeddedSceneObjects : UnloadSceneOptions.None);
+            while (!asyncOperation.isDone)
+            {
+                EventCenter.Instance.EventTrigger("卸载进度", asyncOperation.progress);
+                yield return asyncOperation.progress;
+            }
             callback?.Invoke();
         }
     }
